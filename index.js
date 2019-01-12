c4bad075fc7bdb3dcac1891bfb9a21ddb7945fb3
beneficial-kernel-0493 <fillanlee@aol.com>
2019-01-12 14:36:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var recipes = {};
 //   return objectClone;
 // }
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {[key]: value});
+  return Object.assign({}, object, { [key]: value } ) ;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
