4ac0d2013bc5ae406fd6a773c666726793998a89
beneficial-kernel-0493 <fillanlee@aol.com>
2019-01-12 15:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,3 @@ function destructivelyDeleteFromObjectByKey(object, key){
   delete  object[key];
   return object;
 }
-// function updateObjectWithKeyAndValue(object, key, value){
-// object[key] =  value;
-// return object;
-// }
